15e10d8b017871ab498c0012418cec108c35c39c
Prince Merluza <zero.bash@gmail.com>
2022-05-30 14:03:26 +0800

    Add description to email_flow module

--- components/genesys-email-flow/modules/email_flow/main.tf
+++ components/genesys-email-flow/modules/email_flow/main.tf
@@ -8,6 +8,13 @@ terraform {
   }
 }
 
+#
+# Description:  
+#
+# Uploads the yaml file as an Inbound Email Flow in Genesys Cloud.
+# The flow includes data actions and a queue, so this module depends on the creation of both.
+# 
+
 resource "genesyscloud_flow" "test-flow" {
   filepath = "./EmailComprehendFlow.yaml"
 }
